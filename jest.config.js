f96ee5abf9cf238c5f02e0646bec74586c9bb23e
marcusDevGit <marcus_psc@hotmail.com>
2025-05-05 14:04:33 -0300

    test(config): atualiza configuração do Jest
    - Adiciona cobertura de testes
    - Configura diretório de relatórios
    - Define padrões de exclusão de arquivos

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 // filepath:/test-ci-aula/jest.config.mjs
 export default {
     testEnvironment: "node",
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: ["text", "lcov", "json"],
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/coverage/**",
+        "!jest.config.js"
+    ]
 };
